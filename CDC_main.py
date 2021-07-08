8c3a6197ff5b5a4ee10f788938dce152d47e94f4
ThOpaque <85796483+ThOpaque@users.noreply.github.com>
2021-07-08 04:06:58 +0200

    Add files via upload

---
+++ CDC_main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 22:10:40 2021
+
+@author: thierry
+"""
+
+import CDC_GUI
+
+root = CDC_GUI.Root()
+root.title("Jeu du Cul de Chouette")
+# root1.wm_iconbitmap(r'/Users/thierry/Documents/Python_divers/CDC/owl1.ico')
+root.mainloop()
+
+
+
+
+## a printer dans ReOrganizes
+        # #### Pour se rappeler des variables, printer ça
+        # for k in range(len(self.ListeJoueurs)):
+        #     print('Itération : ',k)
+        #     # print('Joueurs_obj >>> ', self.Joueurs_obj[k])
+        #     print('Root.Joueurs_obj.nom >>> ', Root.Joueurs_obj[k].nom)
+        #     print('Joueurs_obj.nom >>> ', self.Joueurs_obj[k].nom)
+        #     # print('ListeJoueurs >>> ', self.ListeJoueurs[k])
